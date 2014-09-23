c4a70498e6bee5cbdec253765e951048c5d0f02d
webpatch <webpatch@sina.com>
2014-09-23 12:14:32 +0800

    Update and rename uwsgi to uwsgi.sh

---
+++ uwsgi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+apt-get install -y build-essential python-dev
+wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py 
+python get-pip.py
+pip install uwsgi
+
+read -p "Install Python Component [Pillow & markdown & Flask] ?(y/n):" is_p
+if ["$is_p" = "y"];then
+  apt-get install -y libtiff4-dev libjpeg62-dev zlib1g-dev libfreetype6-dev libpng12-dev
+  pip install pillow
+  pip install markdown
+  pip install flask
+fi
